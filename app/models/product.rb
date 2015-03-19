978cc3f7d3585e15ab9022ce6f1c8decf78d241c
Sedikh Dmitriy <sdv39@fire19.msiu.ru>
2015-03-19 16:25:23 +0300

    семинар

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,4 +2,13 @@ class Product < ActiveRecord::Base
   validates :name, presence: true, length: {maximum: 200}
   validates :price, presence: true, numericality: {greater_than: 0}
   validates :weight, numericality: {only_integer: true, greater_than: 0}, allow_blank: true
+  has_many :line_items, dependent: :nullify
+
+  before_destroy :can_destroy?
+
+  def can_destroy?
+      line_items.blank?
+  end
+
+
 end
